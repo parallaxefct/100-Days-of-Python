8f0c9000d119501ba13d331d95500a5a7a8673ae
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-02-14 23:48:30 -0500

    Create choice2_left_apartment.py

---
+++ _Projects/_choose_adv/choice2_left_apartment.py
@@ -0,0 +1,14 @@
+import os
+import found_items
+
+def left():
+    print("\n\n\nYou start running.")
+    print("\nAs you're headed down the hallway you pass your neighbors apartment, the door slightly ajar. You remember they are prepers. The \"preparing for dooms-day\" type.")
+    choice = input("\nDo you go in? ")
+    choice = choice.lower()
+
+    if choice == 'yes':
+        found_items.insideApartment()
+
+    #else:
+        #keepGoing()
