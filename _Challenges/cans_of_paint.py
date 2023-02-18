8027c88372b5448c32e89c8d2bc5abf0a8263f03
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-02-17 22:46:06 -0500

    Projects added

---
+++ _Challenges/cans_of_paint.py
@@ -0,0 +1,7 @@
+#figure out how many cans of paint are needed, provided given surface areas
+
+
+test_h = int(input('Height of wall: '))
+test_w = int(input('Width of wall: '))
+coverage = 5
+paint_calc(height = test_h, width = test_w, cover = coverage)
---
+++ _Projects/_caesar_cipher/caesar_cipher1.py
@@ -0,0 +1,1 @@
+#this project will look create Caesar Cipher
