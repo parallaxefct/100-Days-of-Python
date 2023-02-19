f554ad50538851cd6f5bd1e436cd50bfea429e81
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-02-19 14:47:26 -0500

    Update prime_number.py

--- _Challenges/prime_number.py
+++ _Challenges/prime_number.py
@@ -1,10 +1,11 @@
 #prime number checker algorithm: simply check to see if any given number is a prime number
 def prime_checker(number):
     if number > 1:
-        
+        for index in range(2, number):
+            if number % index:
+                print('This ')
+
 
 
 n = int(input("Check this number: "))
 prime_checker(number=n)
-
-nat = [2, 3, 4, 5, 6, 7, 8, 9, 10]
