62499b6b21ee70b41fd4d99a431c5cbcc905474e
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-02-19 13:50:50 -0500

    resolved bug and started new project

--- _Challenges/cans_of_paint.py
+++ _Challenges/cans_of_paint.py
@@ -1,11 +1,11 @@
 #figure out how many cans of paint are needed, provided given surface areas
 
-def paint_calc():
-    numbers_of_cans = (height * width) / 5
+def paint_calc(height, width, cover):
+    numbers_of_cans = (height * width) / cover
     numbers_of_cans = round(numbers_of_cans)
     print(f'You\'ll need {numbers_of_cans} cans of paint.')
 
-test_h = int(input('Height of wall: '))
-test_w = int(input('Width of wall: '))
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
 coverage = 5
-paint_calc(height = test_h, width = test_w, cover = coverage)
+paint_calc(height=test_h, width=test_w, cover=coverage)
---
+++ _Challenges/prime_number.py
@@ -0,0 +1,10 @@
+#prime number checker algorithm: simply check to see if any given number is a prime number
+def prime_checker(number):
+    if number > 1:
+        
+
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+nat = [2, 3, 4, 5, 6, 7, 8, 9, 10]
