d01ec3e738e7c453edd700fe1b35b42fb33d52bb
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-02-19 14:49:38 -0500

    Update prime_number.py

--- _Challenges/prime_number.py
+++ _Challenges/prime_number.py
@@ -7,7 +7,8 @@ def prime_checker(number):
             else:
                 print('It\'s a prime number.')
 
-
+    else:
+        print('It\'s not a prime number.')
 
 n = int(input("Check this number: "))
 prime_checker(number=n)
