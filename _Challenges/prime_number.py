0874ccd12a574dc1c49940cb77dcf71c2fbb137f
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-03-02 14:19:05 -0500

    Update prime_number.py

--- _Challenges/prime_number.py
+++ _Challenges/prime_number.py
@@ -2,8 +2,13 @@
 def prime_checker(number):
     #this filters 1 and below
     if number > 1:
-        even = number % 2
-        print(even)
+        if number % 2 == 0:
+            print('It\'s not a prime number')
+        elif number % 3 == 0:
+            print('It\'s not a prime number')
+        else:
+            print('It\'s a prime number')
+
 
 n = int(input("Check this number: "))
 prime_checker(number=n)
