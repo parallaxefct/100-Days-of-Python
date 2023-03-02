7501b5f71b7d7336fc75d4efe432e2041e2a68e2
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-03-02 14:30:11 -0500

    Update caesar_cipher1.py

--- _Projects/_caesar_cipher/caesar_cipher1.py
+++ _Projects/_caesar_cipher/caesar_cipher1.py
@@ -8,9 +8,11 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(dir, text, shift):
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
     #e.g.
+
     #plain_text = "hello"
     #shift = 5
     #cipher_text = "mjqqt"
