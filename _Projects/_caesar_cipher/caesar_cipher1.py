3ce1ac8cf048bdfcbc71d8d962c32eec23e6b13c
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-03-03 00:11:36 -0500

    Update caesar_cipher1.py

--- _Projects/_caesar_cipher/caesar_cipher1.py
+++ _Projects/_caesar_cipher/caesar_cipher1.py
@@ -3,13 +3,18 @@
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = lower(input("Type 'encode' to encrypt, type 'decode' to decrypt:\n"))
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(dir, text, shift):
+def encrypt(t, s):
+    source = []
 
+    for i in t:
+        source.append(i)
+
+    print(source)
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
     #e.g.
 
@@ -24,3 +29,7 @@ def encrypt(dir, text, shift):
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+if direction == 'encode':
+    encrypt(text=t, shift=s)
+
+#else:
