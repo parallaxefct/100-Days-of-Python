f39f4bd7f324400e2cc0cf122a75ac2ae9bb31e7
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-03-04 09:07:52 -0500

    Update caesar_cipher1.py

--- _Projects/_caesar_cipher/caesar_cipher1.py
+++ _Projects/_caesar_cipher/caesar_cipher1.py
@@ -9,6 +9,8 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(t, s):
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+    #e.g.
     source_text = []
     encrypted_text  = []
     length_text = len(alphabet)
@@ -26,21 +28,9 @@ def encrypt(t, s):
                 y = y + s
                 encrypted_text.append(alphabet[y])
 
-
-    print(source_text)
-    print(encrypted_text)
-
-
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-    #e.g.
-
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+    #this joins the list to a STRING and outputs the sentence requested
+    encrypted_text = ''.join(encrypted_text)
+    print(f'\nThe encoded text is {encrypted_text}\n')
 
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
