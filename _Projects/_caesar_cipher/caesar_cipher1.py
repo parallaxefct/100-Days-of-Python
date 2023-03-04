3d7812dfb49b20f61a5fb6aae1b6d336652e13f1
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-03-04 08:53:33 -0500

    Update caesar_cipher1.py

--- _Projects/_caesar_cipher/caesar_cipher1.py
+++ _Projects/_caesar_cipher/caesar_cipher1.py
@@ -9,17 +9,25 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(t, s):
-    source = []
-    encrypted  = []
-    length = len(alphabet[])
-    #creates a list for the text input
+    source_text = []
+    encrypted_text  = []
+    length_text = len(alphabet)
+
+    #creates a list for the word given
     for i in t:
-        source.append(i)
+        source_text.append(i)
 
-    for x in range(0, :
-        print(x)
 
-    print(source)
+    for x in source_text:
+
+        for y in range(0, length_text):
+
+            if x == alphabet[y]:
+                append.encrypted_text(alphabet[y + s])
+
+
+    print(source_text)
+    print(encrypted_text)
 
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
@@ -37,6 +45,6 @@ def encrypt(t, s):
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
 if direction == 'encode':
-    encrypt(text=t, shift=s)
+    encrypt(text, shift)
 
 #else:
