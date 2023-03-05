1923ee6c8f14449a97622a67773bb7b9658a0e9f
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-03-05 13:43:31 -0500

    part 2

--- _Projects/_caesar_cipher/caesar_cipher1.py
+++ _Projects/_caesar_cipher/caesar_cipher1.py
@@ -9,7 +9,6 @@ shift = int(input("Type the shift number:\n"))
 
 
 def encrypt(t, s):
-
     #estalishes varible
     encrypted_text  = ''
     length_text = len(alphabet)
@@ -27,7 +26,17 @@ def encrypt(t, s):
     encrypted_text = ''.join(encrypted_text)
     print(f'\nThe encoded text is {encrypted_text}\n')
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+  #e.g.
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
 
 
 if direction == 'encode':
--- _Projects/_caesar_cipher/caesar_cipher_index.py
+++ _Projects/_caesar_cipher/caesar_cipher_index.py
@@ -8,18 +8,7 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(given_text, shift_amount):
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-    #e.g.
-
-    #source_text = []
-    #encrypted_text  = []
-    #length_text = len(alphabet)
-
-    #creates a list for the word given
-    #for i in t:
-        #source_text.append(i)
 
     #this assigns the each letter to X varibale
     for x in given_text:
@@ -40,9 +29,18 @@ def encrypt(given_text, shift_amount):
     encrypted_text = ''.join(encrypted_text)
     print(f'\nThe encoded text is {encrypted_text}\n')
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+  #e.g.
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+
 if direction == 'encode':
     encrypt(given_text=text, shift_amount=shift)
 
