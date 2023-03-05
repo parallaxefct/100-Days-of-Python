9f6405f448c168fe7614a14e2bd9b7467c02f808
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-03-05 14:16:20 -0500

    Update caesar_cipher2.py

--- _Projects/_caesar_cipher/pt2/caesar_cipher2.py
+++ _Projects/_caesar_cipher/pt2/caesar_cipher2.py
@@ -1,7 +1,7 @@
 #this project will look create Caesar Cipher
 
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
@@ -23,16 +23,12 @@ def encrypt(t, s):
                 encrypted_text += alphabet[y]
 
     #this joins the list to a STRING and outputs the sentence requested
-    encrypted_text = ''.join(encrypted_text)
     print(f'\nThe encoded text is {encrypted_text}\n')
 
-#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 def decrypt(t, s):
     #establishes variable
     decrypted_text = ''
     length_text = len(alphabet)
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
-  #e.g.
 
     #this assigns the first letter of the word given, to LEETTER
     for letter in t:
@@ -41,20 +37,14 @@ def decrypt(t, s):
             #this checks X against the index of alphabet and adds the encrypted letters based on SHIFT given
             if letter == alphabet[y]:
                 y = y - s
-                encrypted_text += alphabet[y]
+                decrypted_text += alphabet[y]
+
+    print(f'\nThe encoded text is {decrypted_text}\n')
 
 
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
-
-
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
 
 
 if direction == 'encode':
     encrypt(text, shift)
-
 else:
     decrypt(text, shift)
