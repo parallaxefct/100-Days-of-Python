7d56bea1ed2293717ac1e30a24d523b5b960ca56
parallaxefct <120540334+parallaxefct@users.noreply.github.com>
2023-03-10 10:49:58 -0500

    Update caesar_cipher_index4.py

--- _Projects/_caesar_cipher/pt4/caesar_cipher_index4.py
+++ _Projects/_caesar_cipher/pt4/caesar_cipher_index4.py
@@ -4,8 +4,9 @@ def caesar(start_text, shift_amount, cipher_direction):
   end_text = ""
   if cipher_direction == "decode":
     shift_amount *= -1
-    
+
   for char in start_text:
+    #this if statement ensures that if the character is not in alphabet it will do nothing with it
     if char in alphabet:
         position = alphabet.index(char)
         new_position = position + shift_amount
@@ -13,7 +14,7 @@ def caesar(start_text, shift_amount, cipher_direction):
 
   print(f"Here's the {cipher_direction}d result: {end_text}")
 
-#TODO-1: Import and print the logo from art.py when the program starts.
+#Imports and prints the logo from art.py when the program starts.
 import cipher_art
 print(cipher_art.logo)
 
@@ -30,7 +31,7 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
+#this if statement ensures that regardless of the SHIFT amount that is input, the program will stay within the 26 characters in the alphabet
 if shift > len(alphabet):
     shift = shift % 26
 
